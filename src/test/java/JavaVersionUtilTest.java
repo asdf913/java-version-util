c548451f2f87133fea82a7aa8e43ca459d23e308
lau-ka-ho <asdf913@gmail.com>
2026-05-06 19:17:29 +0900

    Add "org.junit.I"gnore" annotation to "testGetJavaVersionAsInt()" method in "JavaVersionUtilTest" class

--- src/test/java/JavaVersionUtilTest.java
+++ src/test/java/JavaVersionUtilTest.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Method;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JavaVersionUtilTest {
@@ -21,6 +22,7 @@ public class JavaVersionUtilTest {
 	}
 
 	@Test
+	@Ignore
 	public void testGetJavaVersionAsInt() throws IOException {
 		//
 		final Integer major = getClassMajorNumber();
